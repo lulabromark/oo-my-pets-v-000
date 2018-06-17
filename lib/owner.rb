79b3ed2cc4eb90456c5ecf149469b09ffd7bf15c
Mark Lawrence <lawrence.ml@gmail.com>
2018-06-17 02:36:21 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -41,9 +41,9 @@ class Owner
     @pets[:fishes].each { |fish| fish.mood = 'happy' }
   end
 
-  def sell_pets
-    # @pets.each { |type, pets| pet.mood = 'nervous' }
-  end
+  # def sell_pets
+  #   @pets.each { |type, pets| pet.mood = 'nervous' }
+  # end
 
   def list_pets
     return "I have #{@pets[:fishes].count} fish, #{@pets[:dogs].count} dog(s), and #{@pets[:cats].count} cat(s)."
